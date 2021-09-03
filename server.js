e51c948f61c757e2cbfeb18d2734610a5a1f2ad9
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-09-03 13:13:16 -0400

    connection and schema set up

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
+// const routes = require('./controllers');
 const helpers = require('./utils/helpers');
+
 
 const sequelize = require('./config/connection');
 
@@ -15,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: process.env.SESSION_SECRET,
     cookie: {
         // Two hours in milliseconds
         maxAge: 7200000
@@ -36,7 +37,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
