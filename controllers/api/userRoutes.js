8c5b002bd390b99c1526f609491365f2123b7598
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-09-06 16:00:52 -0400

    user login established

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -20,4 +20,36 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Login attempt
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again.' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again.' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.loggedIn = true;
+
+            res.status(200).json({ message: 'You are now logged in' });
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+    }
+})
+
 module.exports = router;
