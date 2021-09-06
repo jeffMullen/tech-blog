49e2291aef6bc3b695ae879ae3108843cad7f918
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-09-06 12:23:11 -0400

    getting post by id

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,7 +15,35 @@ router.get('/', async (req, res) => {
         console.log(e);
         res.status(500);
     }
+})
 
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: { exclude: ['password'] }
+                },
+                {
+                    model: Comment,
+                    include: [{
+                        model: User,
+                        attribues: { exclude: 'password' }
+                    }]
+                }
+            ]
+        });
+
+        // const post = postData.map(details => details.get({ plain: true }));
+        const post = postData.get({ plain: true });
+        res.status(200).json(post);
+        // render to homepage
+
+    } catch (e) {
+        console.log(e);
+        res.status(500);
+    }
 })
 
 module.exports = router;
