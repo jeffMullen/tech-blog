a2e942cd43718a3d779830301344ca92ee21eaef
Walrus <81767572+jeffMullen@users.noreply.github.com>
2021-09-09 16:44:32 -0400

    can both update and delete posts on user dashboard

--- public/js/alterPost.js
+++ public/js/alterPost.js
@@ -1,14 +1,38 @@
 const submitUpdate = document.querySelector('#updatePost');
 const submitDelete = document.querySelector('#deletePost');
+const editPost = document.querySelector('.post-heading');
+
+const postId = editPost.getAttribute('data-id');
+const userId = editPost.getAttribute('data-user-id');
 
 const updatePost = async (event) => {
     event.preventDefault();
+
+    let title = document.querySelector('#newPostTitle').value.trim();
+    const content = document.querySelector('#newPostContent').value.trim();
+
+    if (title === '') {
+        title = document.querySelector('#former-title').innerHTML;
+    }
+
+    const postUpdate = await fetch(`/api/dashboard/posts/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title: title,
+            content: content
+        }),
+        headers: { 'Content-Type': 'application/json' }
+    })
+
+    if (postUpdate.ok) {
+        document.location.replace(`/dashboard/${userId}`);
+    } else {
+        alert('Failed to update post')
+    }
 }
 
 const deletePost = async (event) => {
     event.preventDefault();
-    const postId = event.target.getAttribute('data-id');
-    const userId = event.target.getAttribute('data-user-id');
 
     const response = await fetch(`/api/dashboard/posts/${postId}`, {
         method: 'DELETE',
